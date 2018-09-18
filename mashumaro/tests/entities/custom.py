7ce4d16dd9410abec18b505a50dc998dd9ba0f43
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2018-09-18 22:43:25 +0300

    Add some entities for tests

---
+++ mashumaro/tests/entities/custom.py
@@ -0,0 +1,195 @@
+from collections import deque, ChainMap
+
+from mashumaro.abc import SerializableSequence, SerializableMapping
+from mashumaro.tests.entities.abstract import *
+
+
+class CustomSerializableList(list, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = []
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = list(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableList({str(self.x)})"
+
+
+class CustomSerializableDeque(deque, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = deque()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = deque(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableDeque({str(self.x)})"
+
+
+class CustomSerializableTuple(tuple, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = tuple()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = tuple(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableTuple({str(self.x)})"
+
+
+class CustomSerializableSet(set, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = set()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = set(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableSet({str(self.x)})"
+
+
+class CustomSerializableFrozenSet(set, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = frozenset()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = frozenset(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableFrozenSet({str(self.x)})"
+
+
+class CustomSerializableChainMap(ChainMap, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = ChainMap()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = ChainMap(*seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableChainMap({str(self.x)})"
+
+
+class CustomSerializableMapping(dict, SerializableMapping):
+    def __init__(self):
+        super().__init__()
+        self.x = {}
+
+    @classmethod
+    def from_mapping(cls, mapping):
+        inst = cls()
+        inst.x = {**mapping}
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableMapping({str(self.x)})"
+
+
+class CustomSerializableBytes(bytes, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = bytes()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = bytes(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableBytes({str(self.x)})"
+
+
+class CustomSerializableByteArray(bytes, SerializableSequence):
+    def __init__(self):
+        super().__init__()
+        self.x = bytearray()
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = bytearray(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableByteArray({str(self.x)})"
+
+
+class CustomSerializableSequence(AbstractSequence, SerializableSequence):
+    def __init__(self):
+        self.x = []
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+    def foo(self):
+        pass
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = list(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableSequence({str(self.x)})"
+
+
+class CustomSerializableMutableSequence(AbstractMutableSequence,
+                                        SerializableSequence):
+    def __init__(self):
+        self.x = []
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def insert(self):
+        pass
+
+    def foo(self):
+        pass
+
+    @classmethod
+    def from_sequence(cls, seq):
+        inst = cls()
+        inst.x = list(seq)
+        return inst
+
+    def __repr__(self):
+        return f"CustomSerializableMutableSequence({str(self.x)})"
---
+++ mashumaro/tests/entities/dataclasses.py
@@ -0,0 +1,198 @@
+import typing
+import collections
+from dataclasses import dataclass
+
+from mashumaro import DataClassDictMixin
+from mashumaro.tests.entities.abstract import *
+from mashumaro.tests.entities.custom import *
+
+
+# simple types
+@dataclass
+class DataClassInt(DataClassDictMixin):
+    x: int
+
+
+# list
+@dataclass
+class DataClassWithList(DataClassDictMixin):
+    x: list
+
+
+@dataclass
+class DataClassWithGenericList(DataClassDictMixin):
+    x: typing.List[int]
+
+
+@dataclass
+class DataClassWithCustomSerializableList(DataClassDictMixin):
+    x: CustomSerializableList
+
+
+# deque
+@dataclass
+class DataClassWithDeque(DataClassDictMixin):
+    x: collections.deque
+
+
+@dataclass
+class DataClassWithGenericDeque(DataClassDictMixin):
+    x: typing.Deque[int]
+
+
+@dataclass
+class DataClassWithCustomSerializableDeque(DataClassDictMixin):
+    x: CustomSerializableDeque
+
+
+# tuple
+@dataclass
+class DataClassWithTuple(DataClassDictMixin):
+    x: tuple
+
+
+@dataclass
+class DataClassWithGenericTuple(DataClassDictMixin):
+    x: typing.Tuple[int]
+
+
+@dataclass
+class DataClassWithCustomSerializableTuple(DataClassDictMixin):
+    x: CustomSerializableTuple
+
+
+# set
+@dataclass
+class DataClassWithSet(DataClassDictMixin):
+    x: set
+
+
+@dataclass
+class DataClassWithGenericSet(DataClassDictMixin):
+    x: typing.Set[int]
+
+
+@dataclass
+class DataClassWithCustomSerializableSet(DataClassDictMixin):
+    x: CustomSerializableSet
+
+
+@dataclass
+class DataClassWithAbstractSet(DataClassDictMixin):
+    x: AbstractSet
+
+
+@dataclass
+class DataClassWithAbstractMutableSet(DataClassDictMixin):
+    x: AbstractMutableSet
+
+
+# frozenset
+@dataclass
+class DataClassWithFrozenSet(DataClassDictMixin):
+    x: frozenset
+
+
+@dataclass
+class DataClassWithGenericFrozenSet(DataClassDictMixin):
+    x: typing.FrozenSet[int]
+
+
+@dataclass
+class DataClassWithCustomSerializableFrozenSet(DataClassDictMixin):
+    x: CustomSerializableFrozenSet
+
+
+# ChainMap
+@dataclass
+class DataClassWithChainMap(DataClassDictMixin):
+    x: collections.ChainMap
+
+
+@dataclass
+class DataClassWithGenericChainMap(DataClassDictMixin):
+    x: typing.ChainMap[int, int]
+
+
+@dataclass
+class DataClassWithCustomSerializableChainMap(DataClassDictMixin):
+    x: CustomSerializableChainMap
+
+
+# dict
+@dataclass
+class DataClassWithDict(DataClassDictMixin):
+    x: dict
+
+
+@dataclass
+class DataClassWithGenericDict(DataClassDictMixin):
+    x: typing.Dict[int, int]
+
+
+@dataclass
+class DataClassWithCustomSerializableMapping(DataClassDictMixin):
+    x: CustomSerializableMapping
+
+
+@dataclass
+class DataClassWithAbstractMapping(DataClassDictMixin):
+    x: AbstractMapping
+
+
+@dataclass
+class DataClassWithAbstractMutableMapping(DataClassDictMixin):
+    x: AbstractMutableMapping
+
+
+# bytes, bytearray
+@dataclass
+class DataClassWithBytes(DataClassDictMixin):
+    x: bytes
+
+
+@dataclass
+class DataClassWithByteArray(DataClassDictMixin):
+    x: bytearray
+
+
+@dataclass
+class DataClassWithCustomSerializableBytes(DataClassDictMixin):
+    x: CustomSerializableBytes
+
+
+@dataclass
+class DataClassWithCustomSerializableByteArray(DataClassDictMixin):
+    x: CustomSerializableByteArray
+
+
+@dataclass
+class DataClassWithAbstractByteString(DataClassDictMixin):
+    x: AbstractByteString
+
+
+# str
+@dataclass
+class DataClassWithStr(DataClassDictMixin):
+    x: str
+
+
+# sequence
+@dataclass
+class DataClassWithCustomSerializableSequence(DataClassDictMixin):
+    x: CustomSerializableSequence
+
+
+@dataclass
+class DataClassWithCustomSerializableMutableSequence(DataClassDictMixin):
+    x: CustomSerializableMutableSequence
+
+
+@dataclass
+class DataClassWithAbstractSequence(DataClassDictMixin):
+    x: AbstractSequence
+
+
+@dataclass
+class DataClassWithAbstractMutableSequence(DataClassDictMixin):
+    x: AbstractMutableSequence
