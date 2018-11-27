31dc571fdc784e046e9d6d14470ff8fe24d592ca
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2018-11-27 14:52:58 +0300

    Fix to_yaml method

--- mashumaro/serializer/yaml.py
+++ mashumaro/serializer/yaml.py
@@ -5,7 +5,7 @@ from mashumaro.serializer.base import DataClassDictMixin
 
 class DataClassYAMLMixin(DataClassDictMixin):
     def to_yaml(self):
-        return yaml.dump(self.to_dict(), use_bytes=False)
+        return yaml.dump(self.to_dict(use_bytes=False))
 
     @classmethod
     def from_yaml(cls, data: bytes):
