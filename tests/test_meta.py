fca7a2304804a15e9aa7b14c6715d628297d690b
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2018-11-30 16:18:00 +0300

    Increase coverage

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,8 +1,10 @@
 from unittest.mock import patch
+from dataclasses import dataclass
 
 import pytest
 
 from mashumaro.meta.helpers import is_generic
+from mashumaro import DataClassDictMixin
 
 
 def test_is_generic_unsupported_python():
@@ -10,3 +12,18 @@ def test_is_generic_unsupported_python():
         with patch('mashumaro.meta.helpers.PY_37', False):
             with pytest.raises(NotImplementedError):
                 is_generic(int)
+
+
+def test_no_code_builder():
+    class Mock:
+        def add_from_dict(self):
+            pass
+
+        def add_to_dict(self):
+            pass
+    with patch('mashumaro.serializer.base.metaprogramming.CodeBuilder', Mock):
+        @dataclass
+        class DataClass(DataClassDictMixin):
+            pass
+        assert DataClass.from_dict({}) is None
+        assert DataClass().to_dict() is None
