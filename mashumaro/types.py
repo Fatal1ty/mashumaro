39a0b0869329641136890bf0cb02af9c4d79daea
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-02-22 13:48:11 +0300

    Add RoundedDecimal strategy

--- mashumaro/types.py
+++ mashumaro/types.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import decimal
 
 
 class SerializableType(ABC):
@@ -22,7 +23,29 @@ class SerializationStrategy(ABC):
         pass
 
 
+class RoundedDecimal(SerializationStrategy):
+    def __init__(self, places=None, rounding=None):
+        if places is not None:
+            self.exp = decimal.Decimal((0, (1,), -places))
+        else:
+            self.exp = None
+        self.rounding = rounding
+
+    def _serialize(self, value) -> str:
+        if self.exp:
+            if self.rounding:
+                return str(value.quantize(self.exp, rounding=self.rounding))
+            else:
+                return str(value.quantize(self.exp))
+        else:
+            return str(value)
+
+    def _deserialize(self, value: str) -> decimal.Decimal:
+        return decimal.Decimal(str(value))
+
+
 __all__ = [
     'SerializableType',
     'SerializationStrategy',
+    'RoundedDecimal',
 ]
