aba05b07ec6edc1e4e662c867b14717ec8809880
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-03-16 19:45:28 +0300

    Fix type hints for DataClassJSONMixin

--- mashumaro/serializer/json.py
+++ mashumaro/serializer/json.py
@@ -1,5 +1,5 @@
 import json
-from typing import Union, Optional, Callable, Dict, Mapping
+from typing import Union, Optional, Callable, Dict, Mapping, TypeVar, Type
 from types import MappingProxyType
 
 from mashumaro.serializer.base import DataClassDictMixin
@@ -13,11 +13,12 @@ DEFAULT_DICT_PARAMS = {
 EncodedData = Union[str, bytes, bytearray]
 Encoder = Callable[[Dict], EncodedData]
 Decoder = Callable[[EncodedData], Dict]
+T = TypeVar('T', bound='DataClassJSONMixin')
 
 
 class DataClassJSONMixin(DataClassDictMixin):
     def to_json(
-            self,
+            self: T,
             encoder: Optional[Encoder] = json.dumps,
             dict_params: Optional[Mapping] = MappingProxyType({}),
             **encoder_kwargs) -> EncodedData:
@@ -29,11 +30,11 @@ class DataClassJSONMixin(DataClassDictMixin):
 
     @classmethod
     def from_json(
-            cls,
+            cls: Type[T],
             data: EncodedData,
             decoder: Optional[Decoder] = json.loads,
             dict_params: Optional[Mapping] = MappingProxyType({}),
-            **decoder_kwargs) -> DataClassDictMixin:
+            **decoder_kwargs) -> T:
 
         return cls.from_dict(
             decoder(data, **decoder_kwargs),
