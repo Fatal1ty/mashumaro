6042ff4eea19d94e2e1c494952a6af61ac1ae920
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-03-16 20:04:20 +0300

    Add type hints for DataClassYAMLMixin and DataClassMessagePackMixin

--- mashumaro/serializer/msgpack.py
+++ mashumaro/serializer/msgpack.py
@@ -1,13 +1,45 @@
+from typing import Union, Optional, Callable, Dict, Mapping, TypeVar, Type
+from types import MappingProxyType
+from functools import partial
+
 import msgpack
 
 from mashumaro.serializer.base import DataClassDictMixin
 
 
+DEFAULT_DICT_PARAMS = {
+    'use_bytes': True,
+    'use_enum': False,
+    'use_datetime': False
+}
+EncodedData = Union[str, bytes, bytearray]
+Encoder = Callable[[Dict], EncodedData]
+Decoder = Callable[[EncodedData], Dict]
+T = TypeVar('T', bound='DataClassMessagePackMixin')
+
+
 class DataClassMessagePackMixin(DataClassDictMixin):
-    def to_msgpack(self):
-        d = self.to_dict(use_bytes=True)
-        return msgpack.packb(d, use_bin_type=True)
+    def to_msgpack(
+            self: T,
+            encoder: Optional[Encoder] = partial(
+                msgpack.packb, use_bin_type = True
+            ),
+            dict_params: Optional[Mapping] = MappingProxyType({}),
+            **encoder_kwargs) -> EncodedData:
+
+        return encoder(
+            self.to_dict(**dict(DEFAULT_DICT_PARAMS, **dict_params)),
+            **encoder_kwargs
+        )
 
     @classmethod
-    def from_msgpack(cls, data: bytes):
-        return cls.from_dict(msgpack.unpackb(data, raw=False), use_bytes=True)
+    def from_msgpack(
+            cls: Type[T],
+            data: EncodedData,
+            decoder: Optional[Decoder] = partial(msgpack.unpackb, raw = False),
+            dict_params: Optional[Mapping] = MappingProxyType({}),
+            **decoder_kwargs) -> T:
+        return cls.from_dict(
+            decoder(data, **decoder_kwargs),
+            **dict(DEFAULT_DICT_PARAMS, **dict_params)
+        )
--- mashumaro/serializer/yaml.py
+++ mashumaro/serializer/yaml.py
@@ -1,12 +1,42 @@
+from typing import Union, Optional, Callable, Dict, Mapping, TypeVar, Type
+from types import MappingProxyType
+
 import yaml
 
 from mashumaro.serializer.base import DataClassDictMixin
 
 
+DEFAULT_DICT_PARAMS = {
+    'use_bytes': False,
+    'use_enum': False,
+    'use_datetime': False
+}
+EncodedData = Union[str, bytes]
+Encoder = Callable[[Dict], EncodedData]
+Decoder = Callable[[EncodedData], Dict]
+T = TypeVar('T', bound='DataClassYAMLMixin')
+
+
 class DataClassYAMLMixin(DataClassDictMixin):
-    def to_yaml(self):
-        return yaml.dump(self.to_dict(use_bytes=False))
+    def to_yaml(
+            self: T,
+            encoder: Optional[Encoder] = yaml.dump,
+            dict_params: Optional[Mapping] = MappingProxyType({}),
+            **encoder_kwargs) -> EncodedData:
+
+        return encoder(
+            self.to_dict(**dict(DEFAULT_DICT_PARAMS, **dict_params)),
+            **encoder_kwargs
+        )
 
     @classmethod
-    def from_yaml(cls, data: bytes):
-        return cls.from_dict(yaml.safe_load(data), use_bytes=False)
+    def from_yaml(
+            cls: Type[T],
+            data: EncodedData,
+            decoder: Optional[Decoder] = yaml.safe_load,
+            dict_params: Optional[Mapping] = MappingProxyType({}),
+            **decoder_kwargs) -> T:
+        return cls.from_dict(
+            decoder(data, **decoder_kwargs),
+            **dict(DEFAULT_DICT_PARAMS, **dict_params)
+        )
