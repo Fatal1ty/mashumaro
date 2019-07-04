78d30e4ee3bd6c27529149019f85b99326921b4c
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-07-04 12:13:54 +0300

    Add a missed test for is_vlass_var

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 import pytest
 
-from mashumaro.meta.helpers import is_generic
+from mashumaro.meta.helpers import is_generic, is_class_var
 from mashumaro import DataClassDictMixin
 
 
@@ -12,6 +12,13 @@ def test_is_generic_unsupported_python():
         with patch('mashumaro.meta.helpers.PY_37', False):
             with pytest.raises(NotImplementedError):
                 is_generic(int)
+
+
+def test_is_class_var_unsupported_python():
+    with patch('mashumaro.meta.helpers.PY_36', False):
+        with patch('mashumaro.meta.helpers.PY_37', False):
+            with pytest.raises(NotImplementedError):
+                is_class_var(int)
 
 
 def test_no_code_builder():
