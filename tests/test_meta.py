84559169199454a3dadc4882029f9f873855291e
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-10-27 16:30:36 +0300

    Add test for is_init_var

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 import pytest
 
-from mashumaro.meta.helpers import is_generic, is_class_var
+from mashumaro.meta.helpers import is_generic, is_class_var, is_init_var
 from mashumaro import DataClassDictMixin
 
 
@@ -21,6 +21,14 @@ def test_is_class_var_unsupported_python():
             with patch('mashumaro.meta.helpers.PY_38', False):
                 with pytest.raises(NotImplementedError):
                     is_class_var(int)
+
+
+def test_is_init_var_unsupported_python():
+    with patch('mashumaro.meta.helpers.PY_36', False):
+        with patch('mashumaro.meta.helpers.PY_37', False):
+            with patch('mashumaro.meta.helpers.PY_38', False):
+                with pytest.raises(NotImplementedError):
+                    is_init_var(int)
 
 
 def test_no_code_builder():
