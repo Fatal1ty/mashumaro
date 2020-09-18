11cfe88b2874808c8cf78209a4bd544ad8ca82c6
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2020-09-18 13:24:21 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mashumaro",
-    version="1.12",
+    version="1.13",
     description="Fast serialization framework on top of dataclasses",
     long_description=open('README.md', encoding='utf8').read(),
     long_description_content_type='text/markdown',
