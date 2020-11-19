39b4281c242566109bf796a959bd1965cdbe74dc
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2020-11-19 23:21:07 +0300

    Fix two tests

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -11,16 +11,18 @@ def test_is_generic_unsupported_python():
     with patch('mashumaro.meta.helpers.PY_36', False):
         with patch('mashumaro.meta.helpers.PY_37', False):
             with patch('mashumaro.meta.helpers.PY_38', False):
-                with pytest.raises(NotImplementedError):
-                    is_generic(int)
+                with patch('mashumaro.meta.helpers.PY_39', False):
+                    with pytest.raises(NotImplementedError):
+                        is_generic(int)
 
 
 def test_is_class_var_unsupported_python():
     with patch('mashumaro.meta.helpers.PY_36', False):
         with patch('mashumaro.meta.helpers.PY_37', False):
             with patch('mashumaro.meta.helpers.PY_38', False):
-                with pytest.raises(NotImplementedError):
-                    is_class_var(int)
+                with patch('mashumaro.meta.helpers.PY_39', False):
+                    with pytest.raises(NotImplementedError):
+                        is_class_var(int)
 
 
 def test_is_init_var_unsupported_python():
