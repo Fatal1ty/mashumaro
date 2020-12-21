d51675e14ab2cd95f978efb342350db82b2f90fe
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2020-12-21 22:35:34 +0300

    Fix CustomPath test

--- tests/entities.py
+++ tests/entities.py
@@ -61,4 +61,4 @@ class CustomPath(PathLike):
         return self._path
 
     def __eq__(self, other):
-        return str(self) == str(other)
+        return str(self).replace('\\', '/') == str(other).replace('\\', '/')
