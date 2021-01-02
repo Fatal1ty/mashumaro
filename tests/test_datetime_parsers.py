0153ec05623ca091ff20f1d6e40879c4bc82b080
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2021-01-02 23:15:52 +0300

    Add test for pendulum

--- tests/test_datetime_parsers.py
+++ tests/test_datetime_parsers.py
@@ -11,3 +11,13 @@ def test_ciso8601_parser():
     assert DataClass.from_dict({"x": "2021-01-02T07:00:00Z"}) == DataClass(
         x=datetime(2021, 1, 2, 7, tzinfo=timezone.utc)
     )
+
+
+def test_pendulum_parser():
+    @dataclass
+    class DataClass(DataClassDictMixin):
+        x: datetime = field(metadata={"engine": "pendulum"})
+
+    assert DataClass.from_dict({"x": "2009-W01"}) == DataClass(
+        x=datetime(2008, 12, 29, tzinfo=timezone.utc)
+    )
