dbt-labs/dbt-mashumaro

implement _serializable_encoders dictionary

mashumaro/types.py
@@ -1,5 +1,16 @@
 import decimal
+from typing import TypeVar, Generic
 
+TV = TypeVar("TV")
+
+class SerializableEncoder(Generic[TV]):
+    @classmethod
+    def _serialize(cls, value):
+        raise NotImplementedError
+
+    @classmethod
+    def _deserialize(cls, value):
+        raise NotImplementedError
 
 class SerializableType:
     def _serialize(self):

Fatal1ty/mashumaro

mashumaro/types.py
@@ -1,4 +1,17 @@
 import decimal
+from typing import Generic, TypeVar
+
+TV = TypeVar("TV")
+
+
+class SerializableEncoder(Generic[TV]):
+    @classmethod
+    def _serialize(cls, value):
+        raise NotImplementedError
+
+    @classmethod
+    def _deserialize(cls, value):
+        raise NotImplementedError
 
 
 class SerializableType: