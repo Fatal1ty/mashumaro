2758443bc6404c99aca09a3ec87a5331e5ba7ef5
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2021-02-23 16:06:29 +0300

    Fix test_field_options_helper

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,8 +1,13 @@
 from mashumaro import field_options
+from mashumaro.types import SerializationStrategy
 
 
 def test_field_options_helper():
-    assert field_options() == {"serialize": None, "deserialize": None}
+    assert field_options() == {
+        "serialize": None,
+        "deserialize": None,
+        "serialization_strategy": None,
+    }
 
     def serialize(x):
         return x  # pragma no cover
@@ -10,10 +15,21 @@ def test_field_options_helper():
     def deserialize(x):
         return x  # pragma no cover
 
-    assert (
-        field_options(
-            serialize=serialize,
-            deserialize=deserialize,
-        )
-        == {"serialize": serialize, "deserialize": deserialize}
-    )
+    class TestSerializationStrategy(SerializationStrategy):  # pragma no cover
+        def deserialize(self, value):
+            return value
+
+        def serialize(self, value):
+            return value
+
+    serialization_strategy = TestSerializationStrategy()
+
+    assert field_options(
+        serialize=serialize,
+        deserialize=deserialize,
+        serialization_strategy=serialization_strategy,
+    ) == {
+        "serialize": serialize,
+        "deserialize": deserialize,
+        "serialization_strategy": serialization_strategy,
+    }
