66d1e7f20f3f9f55458718d31ab68ff3d39c5dee
micah <mwilliamson@pushly.com>
2021-11-10 09:48:03 -0600

    updated test to include encapsulation

--- tests/test_serde_kwargs.py
+++ tests/test_serde_kwargs.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from typing import List
 
 import pytest
 from dataclasses import dataclass, field
@@ -27,43 +28,68 @@ class CustomDateSerdeStrategy(SerializationStrategy):
                 raise TypeError("Unexpected Value")
             return datetime.fromisoformat(value)
 
+@dataclass
+class SubDataClass(DataClassDictMixin):
+    x2: datetime = field(metadata=field_options(
+        serialization_strategy=CustomDateSerdeStrategy()
+    ))
+
+    class Config(BaseConfig):
+        code_generation_options = [PROPAGATE_KWARGS]
+
+@dataclass
+class DataClass(DataClassDictMixin):
+    sdc: List[SubDataClass]
+    x: datetime = field(metadata=field_options(
+        serialization_strategy=CustomDateSerdeStrategy()
+    ))
+
+    class Config(BaseConfig):
+        code_generation_options = [PROPAGATE_KWARGS]
+
 
 def test_passing_kwargs_to_serialize():
-    @dataclass
-    class DataClass(DataClassDictMixin):
-        x: datetime = field(metadata=field_options(
-            serialization_strategy=CustomDateSerdeStrategy()
-        ))
-
-        class Config(BaseConfig):
-            code_generation_options = [PROPAGATE_KWARGS]
-
     target_dt = datetime(2020, 1, 1, tzinfo=timezone.utc)
-    iso_dict = {"x": "2020-01-01T00:00:00+00:00"}
-    unix_dict = {"x": 1577836800}
+    iso_dict = {
+        "x": "2020-01-01T00:00:00+00:00",
+        "sdc": [{
+            "x2": "2020-01-01T00:00:00+00:00"
+        }]
+    }
+    unix_dict = {
+        "x": 1577836800,
+        "sdc": [{
+            "x2": 1577836800
+        }]
+    }
 
-    assert DataClass(target_dt).to_dict() == iso_dict
-    assert DataClass(target_dt).to_dict(unix=True) == unix_dict
+    dc = DataClass(sdc=[SubDataClass(x2=target_dt)], x=target_dt)
+    assert dc.to_dict() == iso_dict
+    assert dc.to_dict(unix=True) == unix_dict
 
 
 def test_passing_kwargs_to_deserialize():
-    @dataclass
-    class DataClass(DataClassDictMixin):
-        x: datetime = field(metadata=field_options(
-            serialization_strategy=CustomDateSerdeStrategy()
-        ))
-
-        class Config(BaseConfig):
-            code_generation_options = [PROPAGATE_KWARGS]
 
     target_dt = datetime(2020, 1, 1, tzinfo=timezone.utc)
-    iso_dict = {"x": "2020-01-01T00:00:00+00:00"}
-    unix_dict = {"x": 1577836800}
+    iso_dict = {
+        "x": "2020-01-01T00:00:00+00:00",
+        "sdc": [{
+            "x2": "2020-01-01T00:00:00+00:00"
+        }]
+    }
+    unix_dict = {
+        "x": 1577836800,
+        "sdc": [{
+            "x2": 1577836800
+        }]
+    }
 
     assert DataClass.from_dict(iso_dict).x == target_dt
+    assert DataClass.from_dict(iso_dict).sdc[0].x2 == target_dt
     with pytest.raises(InvalidFieldValue):
         DataClass.from_dict(iso_dict, unix=True)
 
     assert DataClass.from_dict(unix_dict, unix=True).x == target_dt
+    assert DataClass.from_dict(unix_dict, unix=True).sdc[0].x2 == target_dt
     with pytest.raises(InvalidFieldValue):
         DataClass.from_dict(unix_dict)
