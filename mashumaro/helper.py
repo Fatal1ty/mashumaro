3a02b13edd84f9376fc253c9b700503dac03fba1
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2022-01-21 10:34:45 +0300

    Remove typing module

--- mashumaro/helper.py
+++ mashumaro/helper.py
@@ -1,7 +1,22 @@
 from typing import Any, Callable, Optional, Union
 
+from mashumaro.meta.macros import PEP_586_COMPATIBLE
 from mashumaro.types import SerializationStrategy
-from mashumaro.typing import AnyDeserializationEngine, AnySerializationEngine
+
+if PEP_586_COMPATIBLE:  # type: ignore
+    from typing import Literal  # type: ignore
+else:
+    from typing_extensions import Literal  # type: ignore
+
+
+NamedTupleDeserializationEngine = Literal["as_dict", "as_list"]
+DateTimeDeserializationEngine = Literal["ciso8601", "pendulum"]
+AnyDeserializationEngine = Literal[
+    NamedTupleDeserializationEngine, DateTimeDeserializationEngine
+]
+
+NamedTupleSerializationEngine = Literal["as_dict", "as_list"]
+AnySerializationEngine = NamedTupleSerializationEngine
 
 
 def field_options(
