bea0f848c832e36fa8a09a9cccfe1d604374e0e0
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2022-08-18 21:29:54 +0300

    Add classifier for Python 3.11

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Development Status :: 5 - Production/Stable",
     ],
     license="Apache License, Version 2.0",
