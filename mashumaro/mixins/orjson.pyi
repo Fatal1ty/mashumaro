b973167c41177c74c7039f2d0e241130265aadd5
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2022-10-19 15:11:09 +0200

    Fix orjson stubs

--- mashumaro/mixins/orjson.pyi
+++ mashumaro/mixins/orjson.pyi
@@ -1,13 +1,12 @@
-from typing import Any, Callable, Dict, TypeVar, Union
+from typing import Any, Callable, Dict, Type, TypeVar, Union
 
 import orjson
 
-from mashumaro.core.helpers import ConfigValue as ConfigValue
-from mashumaro.dialect import Dialect as Dialect
-from mashumaro.helper import pass_through as pass_through
-from mashumaro.mixins.dict import DataClassDictMixin as DataClassDictMixin
+from mashumaro.dialect import Dialect
+from mashumaro.mixins.dict import DataClassDictMixin
 
 T = TypeVar("T", bound="DataClassORJSONMixin")
+
 EncodedData = Union[str, bytes, bytearray]
 Encoder = Callable[[Any], EncodedData]
 Decoder = Callable[[EncodedData], Dict[Any, Any]]
@@ -16,14 +15,16 @@ class OrjsonDialect(Dialect):
     serialization_strategy: Any
 
 class DataClassORJSONMixin(DataClassDictMixin):
+    __slots__ = ()
     def to_jsonb(
-        self,
+        self: T,
         encoder: Encoder = orjson.dumps,
+        *,
         orjson_options: int = ...,
         **to_dict_kwargs,
     ) -> bytes: ...
     def to_json(
-        self,
+        self: T,
         encoder: Encoder = orjson.dumps,
         *,
         orjson_options: int = ...,
@@ -31,8 +32,8 @@ class DataClassORJSONMixin(DataClassDictMixin):
     ) -> bytes: ...
     @classmethod
     def from_json(
-        cls,
+        cls: Type[T],
         data: EncodedData,
-        decoder: Decoder = ...,
+        decoder: Decoder = orjson.loads,
         **from_dict_kwargs,
     ) -> T: ...
