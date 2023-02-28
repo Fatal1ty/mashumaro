a50fae37e430bf7b390e1a2eb319590d69aabb5f
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2023-02-28 21:47:54 +0300

    Increase coverage

--- tests/test_jsonschema/test_jsonschema_builder.py
+++ tests/test_jsonschema/test_jsonschema_builder.py
@@ -1,7 +1,9 @@
+import json
 from dataclasses import dataclass
 
-from typing_extensions import TypeVarTuple
+from typing_extensions import Literal, TypeVarTuple
 
+from mashumaro.jsonschema.builder import build_json_schema
 from mashumaro.jsonschema.schema import Instance
 
 Ts = TypeVarTuple("Ts")
@@ -19,3 +21,20 @@ def test_instance():
     instance = Instance(DataClass)
     assert instance.holder_class is DataClass
     assert instance.metadata == {}
+
+
+def test_jsonschema_json_simple():
+    assert json.loads(build_json_schema(int).to_json()) == {"type": "integer"}
+
+
+def test_jsonschema_json_literal_none():
+    @dataclass
+    class DataClass:
+        x: Literal[None] = None
+
+    assert json.loads(build_json_schema(DataClass).to_json()) == {
+        "type": "object",
+        "title": "DataClass",
+        "properties": {"x": {"const": None, "default": None}},
+        "additionalProperties": False,
+    }
