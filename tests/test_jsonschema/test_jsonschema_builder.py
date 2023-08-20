c886b0191e14d3cecfa0dd8f00458d3b91cec6fb
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2023-08-20 21:04:24 +0300

    Fix JSON Schema override for optional fields and dataclasses

--- tests/test_jsonschema/test_jsonschema_builder.py
+++ tests/test_jsonschema/test_jsonschema_builder.py
@@ -27,10 +27,10 @@ def test_instance():
 
     instance = Instance(int)
     assert instance.metadata == {}
-    assert instance.holder_class is None
+    assert instance.owner_class is None
 
     instance = Instance(DataClass)
-    assert instance.holder_class is DataClass
+    assert instance.owner_class is None
     assert instance.metadata == {}
 
 
